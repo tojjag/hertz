ab4ba4815f28e8d91a8f18cbbd08c64d4e4e802d
DaddyJocks <daddyjocks@gmail.com>
2018-05-12 17:11:28 +0300

    Rectification DDL

--- DDL.sql
+++ DDL.sql
@@ -1,6 +1,6 @@
 -- DATA DESCRIPTION LANGUAGE :
 
-drop table database hertz2;
+drop database hertz2;
 create database hertz2;
 use hertz2;
 
@@ -38,23 +38,27 @@ create table Users(
 	mdpUser varchar(50),
 	statusAdmin int
 )Engine=InnoDB;
-	
-create table Panier(
+
+create table Location(
 	ID INTEGER NOT NULL AUTO_INCREMENT primary key,
 	idUser int,
 	foreign key (idUser) references Users(ID)
 )Engine=InnoDB;
 
-create table PanierDetail(
+create table LocationDetail(
 	ID INTEGER NOT NULL AUTO_INCREMENT primary key,
-	idPanier int,
+	idLocation int,
 	quantite int,
 	dateDebut date,
 	dateFin date,
+	idAgenceRetirement int,
+	idAgenceRetour int,
 	pu decimal(10, 2),
 	idVehicule int,
-	foreign key (idPanier) references Panier(ID),
-	foreign key (idVehicule) references Vehicule(ID)
+	foreign key (idLocation) references Location(ID),
+	foreign key (idVehicule) references Vehicule(ID),
+	foreign key (idAgenceRetirement) references Agence(ID),
+	foreign key (idAgenceRetour) references Agence(ID)
 )Engine=InnoDB;
 
 create table BonCommande(
@@ -72,24 +76,11 @@ create table BonCommandeDetail(
 	pu decimal(10,2),
 	dateDebut date,
 	dateFin date,
-	foreign key (idBonCommande) references BonCommande(ID)
-)Engine=InnoDB;
-
-create table Location(
-	ID INTEGER NOT NULL AUTO_INCREMENT primary key,
-	idUser int,
-	foreign key (idUser) references Users(ID)
-)Engine=InnoDB;
-
-create table LocationDetail(
-	ID INTEGER NOT NULL AUTO_INCREMENT primary key,
-	idLocation int,
-	dateDebut date,
-	dateFin date,
-	pu decimal(10, 2),
-	idVehicule int,
-	foreign key (idLocation) references Location(ID),
-	foreign key (idVehicule) references Vehicule(ID)
+	idAgenceRetirement int,
+	idAgenceRetour int,
+	foreign key (idBonCommande) references BonCommande(ID),
+	foreign key (idAgenceRetirement) references Agence(ID),
+	foreign key (idAgenceRetour) references Agence(ID)
 )Engine=InnoDB;
 
 create table BonLivraison(
@@ -104,11 +95,11 @@ create table BonLivraison(
 create table BonLivraisonDetail(
 	ID INTEGER NOT NULL AUTO_INCREMENT primary key,
 	idBonLivraison int,
-	idAgence int,
+	idAgenceRetirement int,
 	prix decimal(10,2),
 	dateLivraison date,
 	foreign key (idBonLivraison) references BonLivraison(ID),
-	foreign key (idAgence) references Agence(ID)
+	foreign key (idAgenceRetirement) references Agence(ID)
 )Engine=InnoDB;
 
 create table TVA(
@@ -149,12 +140,25 @@ CREATE TABLE ModeDePaiement(
 	description varchar(50)
 )Engine=InnoDB;
 
+INSERT INTO ModeDePaiement VALUES(0,'Mobile Money');
+INSERT INTO ModeDePaiement VALUES(0,'Chèque');
+INSERT INTO ModeDePaiement VALUES(0,'Carte');
+INSERT INTO ModeDePaiement VALUES(0,'Espèce');
+
 CREATE TABLE ModeDePaiement2(
 	ID INTEGER NOT NULL AUTO_INCREMENT primary key,
 	idmodedepaiement int not null,
 	description varchar(50),
 	FOREIGN KEY(idmodedepaiement)REFERENCES ModeDePaiement(ID)
 )Engine=InnoDB;
+
+INSERT INTO ModeDePaiement2 VALUES(0,1,'Mvola');
+INSERT INTO ModeDePaiement2 VALUES(0,2,'Orange money');
+INSERT INTO ModeDePaiement2 VALUES(0,3,'Airtel money');
+
+INSERT INTO ModeDePaiement2 VALUES(0,1,'BOA');
+INSERT INTO ModeDePaiement2 VALUES(0,2,'BNI');
+INSERT INTO ModeDePaiement2 VALUES(0,3,'BMOI');
 
 create table Payement(
 	ID INTEGER NOT NULL AUTO_INCREMENT primary key,
@@ -177,4 +181,52 @@ create table PayementDetail(
 	foreign key (idmodedepaiement) references ModeDePaiement(ID),
 	foreign key (idmodedepaiement2) references ModeDePaiement2(ID)
 )Engine=InnoDB;
+
+INSERT into Categorie values(0,'locations',null);
+INSERT into Categorie values(0,'ventes',null);	
+	
+INSERT into Categorie values(0,'Voiture de tourisme',1);
+INSERT into Categorie values(0,'Modele Fun',1);
+INSERT into Categorie values(0,'Green collection',1);
+INSERT into Categorie values(0,'Prestige',1);
+INSERT into Categorie values(0,'Family collection',1);
+INSERT into Categorie values(0,'Utilitaire',1);
+INSERT into Categorie values(0,'Special',1);
+
+INSERT into Categorie values(0,'Prestige',2);
+INSERT into Categorie values(0,'Family collection',2);
+INSERT into Categorie values(0,'Utilitaire',2);
+INSERT into Categorie values(0,'Special',2);
+
+
+
+INSERT into Vehicule values(0,'Fiat 500', 10000, 1, 3, '2018-11-09', 4, '16000km','vt1.jpg');
+INSERT into Vehicule values(0,'Renault Clio', 11000, 1, 3, '2017-11-09', 4, '120000km', 'vt2.jpg');
+INSERT into Vehicule values(0,'Nissan Juke Winter', 13000, 1, 3, '2017-11-19', 4, '122000km', 'vt3.jpg');
+INSERT into Vehicule values(0,'Renault Captur', 20000, 1, 3, '2017-12-29', 5, '50000km', 'vt4.jpg');
+INSERT into Vehicule values(0,'Volkswagen Golf', 21000, 1, 3, '2017-11-19', 4, '30000km', 'vt5.jpg');
+INSERT into Vehicule values(0,'Peugeot 508', 23000, 1, 3, '2017-12-10', 7, '129000km', 'vt6.jpg');
+INSERT into Vehicule values(0,'Peugeot 2008', 23000, 1, 3, '2017-11-22', 5, '120000km', 'vt7.jpg');
+
+
+INSERT into Vehicule values(0,'Fiat 500C', 120000, 1, 4, '2018-01-23', 4, '220000km', 'vf1.jpg');
+INSERT into Vehicule values(0,'Citroen DS3 Softroof', 50000, 1, 4, '2017-10-12', 4, '320000km', 'vf2.jpg');
+INSERT into Vehicule values(0,'Mini Cooper 5', 20000, 1, 4, '2018-01-12', 4, '90000km', 'vf3.jpg');
+INSERT into Vehicule values(0,'Mercedes Class ', 300000, 1, 4, '2017-11-02', 4, '12000km', 'vf4.jpg');
+INSERT into Vehicule values(0,'Audi Q2', 50000, 1, 4, '2017-12-15', 4, '110000km', 'vf5.jpg');
+
+INSERT INTO Users values(0,'Lock to hang', 'Angelo','parkedenne@gmail.com',sha1('qwerty'), 0);
+INSERT INTO Users values(0,'Lock', 'Julio','julio@gmail.com',sha1('azerty'), 0);
+INSERT INTO Users values(0,'Admin', 'admin','admin@gmail.com',sha1('admin'), 1);
+
+INSERT INTO Agence VALUES(0,'Andoharanofotsy');
+INSERT INTO Agence VALUES(0,'Ambanidia');
+INSERT INTO Agence VALUES(0,'Ambohipo');
+INSERT INTO Agence VALUES(0,'Analakely');
+INSERT INTO Agence VALUES(0,'Tamjobato');
+INSERT INTO Agence VALUES(0,'Soarano');
+INSERT INTO Agence VALUES(0,'Mahamasina');
+INSERT INTO Agence VALUES(0,'Itaosy');
+
+INSERT INTO TVA values(0, 20);
 
