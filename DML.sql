4c2e368e7ab8e7899a7ab7d5e7033e1b7fb82d9c
Gimmy <gimmyarazafimbelo2@gmail.com>
2018-05-15 09:40:31 +0300

    MAJ de DDL et DML

--- DML.sql
+++ DML.sql
@@ -16,6 +16,8 @@ insert into Categorie values(0,'Family collection',2);
 insert into Categorie values(0,'Utilitaire',2);
 insert into Categorie values(0,'Special',2);
 
+
+
 insert into Vehicule values(0,'Fiat 500', 10000, 1, 3, '2018-11-09', 4, '16000km','vt1.jpg');
 insert into Vehicule values(0,'Renault Clio', 11000, 1, 3, '2017-11-09', 4, '120000km', 'vt2.jpg');
 insert into Vehicule values(0,'Nissan Juke Winter', 13000, 1, 3, '2017-11-19', 4, '122000km', 'vt3.jpg');
@@ -23,6 +25,7 @@ insert into Vehicule values(0,'Renault Captur', 20000, 1, 3, '2017-12-29', 5, '5
 insert into Vehicule values(0,'Volkswagen Golf', 21000, 1, 3, '2017-11-19', 4, '30000km', 'vt5.jpg');
 insert into Vehicule values(0,'Peugeot 508', 23000, 1, 3, '2017-12-10', 7, '129000km', 'vt6.jpg');
 insert into Vehicule values(0,'Peugeot 2008', 23000, 1, 3, '2017-11-22', 5, '120000km', 'vt7.jpg');
+
 
 insert into Vehicule values(0,'Fiat 500C', 120000, 1, 4, '2018-01-23', 4, '220000km', 'vf1.jpg');
 insert into Vehicule values(0,'Citroen DS3 Softroof', 50000, 1, 4, '2017-10-12', 4, '320000km', 'vf2.jpg');
@@ -34,26 +37,13 @@ INSERT INTO Users values(0,'Lock to hang', 'Angelo','parkedenne@gmail.com',sha1(
 INSERT INTO Users values(0,'Lock', 'Julio','julio@gmail.com',sha1('azerty'), 0);
 INSERT INTO Users values(0,'Admin', 'admin','admin@gmail.com',sha1('admin'), 1);
 
-INSERT INTO Agence VALUES(0, 'Andoharanofotsy');
-INSERT INTO Agence VALUES(0, 'Ambanidia');
-INSERT INTO Agence VALUES(0, 'Ambohipo');
-INSERT INTO Agence VALUES(0, 'Analakely');
-INSERT INTO Agence VALUES(0, 'Tamjobato');
-INSERT INTO Agence VALUES(0, 'Soarano');
-INSERT INTO Agence VALUES(0, 'Mahamasina');
-INSERT INTO Agence VALUES(0, 'Itaosy');
+INSERT INTO Agence VALUES(0,'Andoharanofotsy');
+INSERT INTO Agence VALUES(0,'Ambanidia');
+INSERT INTO Agence VALUES(0,'Ambohipo');
+INSERT INTO Agence VALUES(0,'Analakely');
+INSERT INTO Agence VALUES(0,'Tamjobato');
+INSERT INTO Agence VALUES(0,'Soarano');
+INSERT INTO Agence VALUES(0,'Mahamasina');
+INSERT INTO Agence VALUES(0,'Itaosy');
 
 INSERT INTO TVA values(0, 20);
-
-INSERT INTO ModeDePaiement VALUES(0,'Mobile Money');
-INSERT INTO ModeDePaiement VALUES(0,'Chèque');
-INSERT INTO ModeDePaiement VALUES(0,'Carte');
-INSERT INTO ModeDePaiement VALUES(0,'Espèce');
-
-INSERT INTO ModeDePaiement2 VALUES(0,1,'Mvola');
-INSERT INTO ModeDePaiement2 VALUES(0,2,'Orange money');
-INSERT INTO ModeDePaiement2 VALUES(0,3,'Airtel money');
-
-INSERT INTO ModeDePaiement2 VALUES(0,1,'BOA');
-INSERT INTO ModeDePaiement2 VALUES(0,2,'BNI');
-INSERT INTO ModeDePaiement2 VALUES(0,3,'BMOI');
